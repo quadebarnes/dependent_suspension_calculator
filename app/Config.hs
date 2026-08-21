50cc2e53cc1bcd44c0d2b3d999903f82cd1496b5
Quade Barnes <quade.barnes@gmail.com>
2026-08-21 12:33:37 -0600

    Implement universal coordinate system using normalize function.

--- app/Config.hs
+++ app/Config.hs
@@ -2,12 +2,13 @@
 
 module Config
   ( Config (..),
+    normalize,
   )
 where
 
 import Data.Aeson (FromJSON)
 import GHC.Generics (Generic)
-import Geometry (Point)
+import Geometry (Point (..))
 
 data Config = Config
   { wheelbase :: Double,
@@ -39,3 +40,41 @@ data Config = Config
   deriving (Show, Generic)
 
 instance FromJSON Config
+
+normalizePoint :: Point -> Double -> Point
+normalizePoint rawPoint wb =
+  Point
+    { x = wb - x rawPoint,
+      y = y rawPoint,
+      z = z rawPoint
+    }
+
+normalize :: Config -> Config
+normalize rawCfg =
+  Config
+    { wheelbase = wheelbase rawCfg,
+      brakeBias = brakeBias rawCfg,
+      driveBias = driveBias rawCfg,
+      mass = mass rawCfg,
+      massSprung = massSprung rawCfg,
+      massUnsprung = massUnsprung rawCfg,
+      wheelRollingRadius = wheelRollingRadius rawCfg,
+      frontShockAxleMountingLoc = frontShockAxleMountingLoc rawCfg,
+      frontShockFrameMountingLoc = frontShockFrameMountingLoc rawCfg,
+      frontShockExtendedLength = frontShockExtendedLength rawCfg,
+      frontShockCompressedLength = frontShockCompressedLength rawCfg,
+      rearShockAxleMountingLoc = normalizePoint (rearShockAxleMountingLoc rawCfg) (wheelbase rawCfg),
+      rearShockFrameMountingLoc = normalizePoint (rearShockFrameMountingLoc rawCfg) (wheelbase rawCfg),
+      rearShockExtendedLength = rearShockExtendedLength rawCfg,
+      rearShockCompressedLength = rearShockCompressedLength rawCfg,
+      frontUpperArmFrameMountLoc = frontUpperArmFrameMountLoc rawCfg,
+      frontUpperArmAxleMountLoc = frontUpperArmAxleMountLoc rawCfg,
+      frontLowerArmFrameMountLoc = frontLowerArmFrameMountLoc rawCfg,
+      frontLowerArmAxleMountLoc = frontLowerArmAxleMountLoc rawCfg,
+      frontTrackBarAxleMountLoc = frontTrackBarAxleMountLoc rawCfg,
+      frontTrackBarFrameMountLoc = frontTrackBarFrameMountLoc rawCfg,
+      rearUpperArmFrameMountLoc = normalizePoint (rearUpperArmFrameMountLoc rawCfg) (wheelbase rawCfg),
+      rearUpperArmAxleMountLoc = normalizePoint (rearUpperArmAxleMountLoc rawCfg) (wheelbase rawCfg),
+      rearLowerArmFrameMountLoc = normalizePoint (rearLowerArmFrameMountLoc rawCfg) (wheelbase rawCfg),
+      rearLowerArmAxleMountLoc = normalizePoint (rearLowerArmAxleMountLoc rawCfg) (wheelbase rawCfg)
+    }
